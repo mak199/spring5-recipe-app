005a178aa3d3d65f72ffdf16d09569cd6821e8e2
mak199 <mak.khan199@googlemail.com>
2020-07-05 18:40:32 +0200

    Added Functionality to Show an Ingredient

---
+++ src/main/java/guru/springframework/spring5recipeapp/services/IngredientServiceImpl.java
@@ -0,0 +1,46 @@
+package guru.springframework.spring5recipeapp.services;
+
+import guru.springframework.spring5recipeapp.commands.IngredientCommand;
+import guru.springframework.spring5recipeapp.converters.IngredientToIngredientCommand;
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class IngredientServiceImpl implements  IngredientService {
+
+    private final RecipeRepository recipeRepository;
+    private final IngredientToIngredientCommand ingredientToIngredientCommand;
+
+    public IngredientServiceImpl(RecipeRepository recipeRepository, IngredientToIngredientCommand ingredientToIngredientCommand) {
+        this.recipeRepository = recipeRepository;
+
+        this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+    }
+
+    @Override
+    public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+
+        if(!recipeOptional.isPresent()){
+            log.error("recipe id not found:"+recipeId);
+        }
+
+        Recipe recipe = recipeOptional.get();
+
+        Optional<IngredientCommand> ingredientCommand = recipe.getIngredients().stream()
+                .filter(ingredient->ingredient.getId().equals(ingredientId))
+                .map(ingredient -> ingredientToIngredientCommand.convert(ingredient)).findFirst();
+
+        if(!ingredientCommand.isPresent()){
+            log.error("Ingredient Not Found"+ingredientId);
+        }
+
+        return ingredientCommand.get();
+    }
+}
